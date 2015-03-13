2a8308c493d4b014bf0a933590b462766a68ca4b
nqv1994 <nqv1994@gmail.com>
2015-03-13 16:58:06 -0500

    Fix all modals and submit buttons

--- www/js/native.js
+++ www/js/native.js
@@ -1,18 +1,11 @@
 var goBack = function() {
     window.history.back();
 };
-function modalHeight() {
-	// console.log($(ion-content>div.scroll).height());
-	console.log("readyyy");
-}
-$(window).load(function(){
-	console.log("ready");
-});
 document.addEventListener("deviceready", onDeviceReady, false);
 
 function onDeviceReady() {
     navigator.splashscreen.hide()
-}
+};
 
 //Supposed to make LocalStorage Easier
 Storage.prototype.setObject = function(key, value) {
